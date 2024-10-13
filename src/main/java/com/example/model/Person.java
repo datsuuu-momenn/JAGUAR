9bc1f9fcad4e13448970d97b15604f6ba62eb153
KoheiHara <3kzey8c9gd7@gmail.com>
2024-10-13 14:46:19 +0900

    feat: jsf sample

--- src/main/java/com/example/model/Person.java
+++ src/main/java/com/example/model/Person.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Person {
 
     @Id
@@ -14,20 +20,4 @@ public class Person {
 
     private String name;
 
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
---
+++ src/main/java/com/example/task/Task.java
@@ -0,0 +1,30 @@
+package com.example.task;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@NamedQuery(name = Task.FIND_ALL, query = "SELECT t FROM Task t")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+    public static final String FIND_ALL = "demo.jsf.Task.ALL";
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String title;
+
+}
